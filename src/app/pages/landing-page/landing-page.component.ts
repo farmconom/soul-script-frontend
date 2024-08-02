cc90e98e01a940673b71336b93a3219294e8441d
farm prakasit <farmconom@gmail.com>
2024-08-03 00:26:36 +0700

    add typing effect

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -5,4 +5,28 @@ import { Component } from '@angular/core';
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss',
 })
-export class LandingPageComponent {}
+export class LandingPageComponent {
+  textToType: string = 'Soul Script';
+  displayText: string = '';
+  typingSpeed: number = 150; // Speed in milliseconds
+
+  ngOnInit(): void {
+    this.startTyping();
+  }
+
+  startTyping() {
+    let currentIndex = 0;
+
+    const type = () => {
+      if (currentIndex < this.textToType.length) {
+        this.displayText += this.textToType.charAt(currentIndex);
+        currentIndex++;
+        setTimeout(type, this.typingSpeed);
+      }
+    };
+
+    setTimeout(() => {
+      type();
+    }, 200);
+  }
+}
